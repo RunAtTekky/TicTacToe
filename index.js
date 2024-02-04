742b0408ad31c03694342db34470b5aa3738fc2e
Varun <varunrawat343@gmail.com>
2024-02-04 19:11:38 +0530

    [feature] Added Visual Cue on winning the game

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ for (let boxNumber = 0; boxNumber<9; boxNumber++) {
     grid = [];
     boxes.forEach(item => {
       item.innerHTML = '';
+      item.style.opacity = 1;
     })
     document.querySelector('.grid-container').style.opacity = 1;
     xTurn = true;
@@ -87,9 +88,48 @@ function hasSomeoneWon() {
   return false;
 }
 function declareWinner() {
-  document.querySelector('.grid-container').style.opacity = 0;
+  highlightLine();
+  // document.querySelector('.grid-container').style.opacity = 0;
   const winner = (!xTurn) ? 'X Won' : 'O Won';
   console.log(`${winner}`);
   document.querySelector('h1').innerHTML = winner;
 }
 
+function highlightLine() {
+  let boxes_to_highlight = getHighlight();
+  console.log(boxes_to_highlight);
+  boxes_to_highlight.forEach(num => {
+    let box = boxes[num];
+    box.style.opacity = 0.3;
+  });
+}
+
+function getHighlight() {
+  let boxes_to_highlight = [];
+  for (let i=0; i<3; i++) {
+    if (grid[i] !== undefined && grid[i]===grid[i+3] && grid[i]===grid[i+6]) {
+      boxes_to_highlight.push(i);
+      boxes_to_highlight.push(i+3);
+      boxes_to_highlight.push(i+6);
+    }
+  }
+  for (let i=0; i<=6; i+=3) {
+    if (grid[i] !== undefined && grid[i]===grid[i+1] && grid[i]===grid[i+2]) {
+      boxes_to_highlight.push(i);
+      boxes_to_highlight.push(i+1);
+      boxes_to_highlight.push(i+2);
+    }
+  }
+  if (grid[0] !== undefined && grid[0]===grid[4] && grid[0]===grid[8]) {
+    boxes_to_highlight.push(0);
+    boxes_to_highlight.push(4);
+    boxes_to_highlight.push(8);
+  }
+  if (grid[2] !== undefined && grid[2]===grid[4] && grid[4]===grid[6]) {
+    boxes_to_highlight.push(2);
+    boxes_to_highlight.push(4);
+    boxes_to_highlight.push(6);
+  }
+  
+  return boxes_to_highlight;
+}
